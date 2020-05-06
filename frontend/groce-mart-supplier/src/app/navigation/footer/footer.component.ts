0e55c6c683d9f748642a8c88fd2aa721adc95072
Naresh Kumar <nareshkumarAtnPLUS@gmail.com>
2020-05-06 11:40:05 +0530

    Theme setup for supplier completed

--- frontend/groce-mart-supplier/src/app/navigation/footer/footer.component.ts
+++ frontend/groce-mart-supplier/src/app/navigation/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  rowHeightDynamic;
   ngOnInit(): void {
+    if(this.isHandset$){
+      this.rowHeightDynamic = 10;
+    } else{
+      this.rowHeightDynamic = 20;
+    }
   }
 
 }
--- frontend/groce-mart-supplier/src/app/navigation/main-nav/main-nav.component.ts
+++ frontend/groce-mart-supplier/src/app/navigation/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -6,7 +6,8 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss']
+  styleUrls: ['./main-nav.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainNavComponent {
 
